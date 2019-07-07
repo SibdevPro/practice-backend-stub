62553fd51ddd21a4fbb252c4a63fe4bb5437d822
davyd_davyd <dav_tema@mail.ru>
2019-07-07 14:48:31 +0700

    add settings DRF

--- server/config/settings/installed_apps.py
+++ server/config/settings/installed_apps.py
@@ -6,6 +6,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 LOCAL_APPS = []
--- server/config/settings/settings.py
+++ server/config/settings/settings.py
@@ -137,3 +137,24 @@ if DEBUG:
     }
     MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware', ] + MIDDLEWARE
     INSTALLED_APPS += ['debug_toolbar', ]
+
+##################################################################
+# REST framework settings
+##################################################################
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'url_filter.integrations.drf.DjangoFilterBackend',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
+
+if DEBUG:
+    del REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES']
