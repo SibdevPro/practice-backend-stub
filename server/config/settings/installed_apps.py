f93cefc376a11bd5005618a13136e430e6ea81a2
davyd_davyd <dav_tema@mail.ru>
2019-07-07 14:25:35 +0700

    init application

---
+++ server/config/settings/installed_apps.py
@@ -0,0 +1,19 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+]
+
+LOCAL_APPS = []
+
+INSTALLED_APPS += LOCAL_APPS
+
+LOCAL_MIGRATIONS = [app_path.split('.')[1] for app_path in LOCAL_APPS]
+
+MIGRATION_PATH = 'config.migrations.'
+
+MIGRATION_MODULES = {app_name: MIGRATION_PATH + app_name for app_name in LOCAL_MIGRATIONS}
